4ff4e5f4af9c86a84cb6aa9051fec99527059757
jigme <67099510+jigmegithub@users.noreply.github.com>
2021-05-04 07:09:43 +0600

    Added Files via Upload

---
+++ google_map-contact-us-page.js
@@ -0,0 +1,15 @@
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  const hotelNewGrand = { lat: 27.469757471013178, lng: 89.63951046838194 };
+  // The map, centered at Hotel New Grand
+  const map = new google.maps.Map(document.querySelector(".hotel-map-conact-us-page"), {
+    zoom: 16.5,
+    center: hotelNewGrand,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: hotelNewGrand,
+    map: map,
+  });
+}
